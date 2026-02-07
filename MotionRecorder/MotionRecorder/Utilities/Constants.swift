bba97a7b1f07850f0466a1abdd7b8667d23309c4
dante canizo <dantecanizo@dantes-MacBook-Pro-2.local>
2026-02-07 16:52:24 -0300

    Adding constants

---
+++ MotionRecorder/MotionRecorder/Utilities/Constants.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+enum Constants {
+    static let samplingFrequency: Double = 50.0
+    static let motionUpdateInterval: TimeInterval = 1.0 / samplingFrequency
+    static let batchInsertSize: Int = Int(samplingFrequency)
+
+    static let databaseFileName = "motion_data.sqlite"
+
+    enum UserDefaultsKeys {
+        static let lastExportTime = "lastExportTime"
+        static let firstRecordingTime = "firstRecordingTime"
+    }
+
+    static let csvFileNamePrefix = "motion_data_export"
+    static let csvFileDateFormat = "yyyyMMdd_HHmmss"
+    static let csvHeader = "timestamp,x,y,z"
+}
